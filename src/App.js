1ce3f953b4448f15e31d8ae3091a8cbbc01c9ded
Michael-B1764 <mikebailar@gmail.com>
2020-02-28 11:43:08 -0800

    done with styling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage.js";
 import CharacterList from "./components/CharacterList";
 import {Route, Link} from "react-router-dom";
+import styled from "styled-components";
+
+const NavLinks = styled.div`
+display:flex;
+justify-content: space-between;
+width: 30%;
+margin-left: 35%;
+margin-bottom: 5%;
+font-size: 30px;
+`
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Link to="/">Home</Link>
-      <br/>
-      <Link to="/characters">Character List</Link>
-
+      <NavLinks>
+        <Link to="/">Home</Link>
+        <Link to="/characters">Character List</Link>
+      </NavLinks>  
       <Route exact path ="/" component={WelcomePage} />
       <Route  path="/characters/" component={CharacterList}/>
     </main>
