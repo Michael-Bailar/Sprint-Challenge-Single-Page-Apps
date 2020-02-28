436e1d4bcf32215d750496676f3b282642700f8c
Michael-B1764 <mikebailar@gmail.com>
2020-02-28 09:31:43 -0800

    added all data and connected routes

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    props.charData.map(char => {
+
+      return (
+        <div key={char.id}>
+          <h3>{char.name}</h3>
+          <p>{char.species}</p>
+          <p>{char.status}</p>
+        </div>
+      )
+    })
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+export default function CharacterList(props) {
+  
+
+  const [charData, setCharData] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      setCharData(response.data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }, []);
+
+
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      <CharacterCard charData={charData} key={charData.id}/>
     </section>
   );
 }
