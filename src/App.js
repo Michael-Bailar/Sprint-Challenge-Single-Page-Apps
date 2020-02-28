7ddb1fd15bf051f7980f0c808e39b49a0bb5f786
Michael-B1764 <mikebailar@gmail.com>
2020-02-28 11:35:29 -0800

    adjusted project to look better.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage.js";
 import CharacterList from "./components/CharacterList";
+import {Route, Link} from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <WelcomePage />
-      <CharacterList  />
+      <Link to="/">Home</Link>
+      <br/>
+      <Link to="/characters">Character List</Link>
+
+      <Route exact path ="/" component={WelcomePage} />
+      <Route  path="/characters/" component={CharacterList}/>
     </main>
   );
 }
